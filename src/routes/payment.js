11807c6e7e53009c7889ac73948b91f82b2149a3
vrahul2698 <vrahulmba005@gmail.com>
2025-10-06 21:27:17 +0530

    isPremium feature

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -84,4 +84,17 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
     }
 })
 
+paymentRouter.get("/premium/verify", AuthSignin, async (req, res) => {
+    try {
+        const user = req?.user;
+        if (user.isPremium) {
+            return res.json({ isPremium: true });
+        }
+        return res.json({ isPremium: false });
+    }
+    catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
+})
+
 module.exports = paymentRouter;
