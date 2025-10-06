73b8f391ae5f7c75ce93798500708cb5af3a5c68
vrahul2698 <vrahulmba005@gmail.com>
2025-10-06 09:12:53 +0530

    bugfix: Payment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -46,7 +46,7 @@ paymentRouter.post("/payment/create", AuthSignin, async (req, res) => {
 
 paymentRouter.post("/payment/webhook", async (req, res) => {
     try {
-        const webHoofSignature = req.get["X-Razorpay-Signature"]
+        const webHoofSignature = req.get("X-Razorpay-Signature")
         const isWebHookValid = validateWebhookSignature(JSON.stringify(req?.body),
             webHoofSignature,
             process.env.RAZORPAY_WEBHOOK_SECRET);
